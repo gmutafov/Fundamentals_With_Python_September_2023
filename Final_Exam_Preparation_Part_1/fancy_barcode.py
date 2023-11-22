4695599586330745c9d9fed8e106ff26583f3605
gmutafov <georgimutafovbg@gmail.com>
2023-11-22 21:15:45 +0200

    Exercises

---
+++ Final_Exam_Preparation_Part_1/fancy_barcode.py
@@ -0,0 +1,21 @@
+import re
+
+
+def validate_barcode(barcode_data):
+    pattern = r'@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+'
+
+    for barcode in barcode_data:
+        match = re.fullmatch(pattern, barcode)
+
+        if match:
+            product_group = ''.join(re.findall(r'\d', barcode))
+            product_group = product_group if product_group else '00'
+            print(f"Product group: {product_group}")
+        else:
+            print('Invalid barcode')
+
+
+number = int(input())
+data = [input() for _ in range(number)]
+
+validate_barcode(data)
