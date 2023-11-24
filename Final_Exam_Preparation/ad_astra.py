b448a12d6152d85fd95fdece356bdfa3937b0d0b
gmutafov <georgimutafovbg@gmail.com>
2023-11-24 21:24:19 +0200

    Exercises

--- Final_Exam_Preparation/ad_astra.py
+++ Final_Exam_Preparation/ad_astra.py
@@ -1,20 +1,36 @@
+# import re
+#
+# pattern = r'(\#|\|)([A-Za-z]+\s?[A-Za-z]+)\1([0-9]{2}\/[0-9]{2}\/[0-9]{2})\1(\d+)\1'
+# text_string = input()
+# matches = re.findall(pattern, text_string)
+#
+# calories = 0
+# products = []
+#
+# for match in matches:
+#     item_name = match[1]
+#     exp_date = match[2]
+#     cals = match[3]
+#     calories += int(cals)
+#     products.append(f'Item: {item_name}, Best before: {exp_date}, Nutrition: {cals}')
+#
+# days_cals = calories // 2000
+#
+# print(f"You have food to last you for: {days_cals} days!")
+# print('\n'.join(products))
 import re
 
-pattern = r'(\#|\|)([A-Za-z]+\s?[A-Za-z]+)\1([0-9]{2}\/[0-9]{2}\/[0-9]{2})\1(\d+)\1'
-text_string = input()
-matches = re.findall(pattern, text_string)
+pattern = r'(?i)([#|])([a-z\s]+)(\1)(\d{2}\/\d{2}\/\d{2})(\1)(\d+)(\1)'
 
-calories = 0
-products = []
+info = input()
+matches = re.findall(pattern, info)
 
-for match in matches:
-    item_name = match[1]
-    exp_date = match[2]
-    cals = match[3]
-    calories += int(cals)
-    products.append(f'Item: {item_name}, Best before: {exp_date}, Nutrition: {cals}')
+total_calories = sum([int(match[5]) for match in matches])
+days = total_calories // 2000
+print(f'You have food to last you for: {days} days!')
 
-days_cals = calories // 2000
-
-print(f"You have food to last you for: {days_cals} days!")
-print('\n'.join(products))
+for element in matches:
+    item_name = element[1]
+    exp_date = element[3]
+    calories = element[5]
+    print(f"Item: {item_name}, Best before: {exp_date}, Nutrition: {calories}")
---
+++ Final_Exam_Preparation/p!rates.py
@@ -0,0 +1,43 @@
+cities = {}
+
+command = input().split('||')
+
+while command[0] != 'Sail':
+    city, population, gold = command[0], int(command[1]), int(command[2])
+    if city not in cities.keys():
+        cities[city] = {'population': 0, 'gold': 0}
+    cities[city]['population'] += population
+    cities[city]['gold'] += gold
+
+    command = input().split('||')
+
+command = input().split('=>')
+
+while command[0] != 'End':
+    action = command[0]
+    if action == 'Plunder':
+        town, people, gold = command[1], int(command[2]), int(command[3])
+        print(f"{town} plundered! {gold} gold stolen, {people} citizens killed.")
+        cities[town]['population'] -= people
+        cities[town]['gold'] -= gold
+        if cities[town]['population'] == 0 or cities[town]['gold'] == 0:
+            cities.pop(town)
+            print(f"{town} has been wiped off the map!")
+
+    elif action == 'Prosper':
+        town, gold = command[1], int(command[2])
+        if gold < 0:
+            print(f"Gold added cannot be a negative number!")
+        else:
+            cities[town]['gold'] += gold
+            print(f"{gold} gold added to the city treasury. {town} now has {cities[town]['gold']} gold.")
+    command = input().split('=>')
+
+if cities:
+    print(f"Ahoy, Captain! There are {len(cities)} wealthy settlements to go to:")
+    for town, town_info in cities.items():
+        people = town_info['population']
+        gold = town_info['gold']
+        print(f"{town} -> Population: {people} citizens, Gold: {gold} kg")
+else:
+    print(f"Ahoy, Captain! All targets have been plundered and destroyed!")
